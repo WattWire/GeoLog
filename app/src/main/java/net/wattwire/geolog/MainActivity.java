3b5d99304880fe5663c4399b0beda4334eb69a86
Dave Watt <gonebikn@gmail.com>
2017-02-07 17:13:11 -0800

    First committ from LapCat at Peets

--- app/src/main/java/net/wattwire/geolog/MainActivity.java
+++ app/src/main/java/net/wattwire/geolog/MainActivity.java
@@ -2,12 +2,24 @@ package net.wattwire.geolog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mConditionText;
+    Button mButtonSunny;
+    Button mButtonRainy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get UI Elements
+
+        mConditionText = findViewById(R.id.C.id)
+
+
     }
 }
