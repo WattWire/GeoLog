1278d6d8049c9a5e09307a6bfccc3cec84ad0eae
Dave Watt <gonebikn@gmail.com>
2017-03-10 12:50:00 -0800

    Success in getting custom text into Firebase.

--- app/src/main/java/net/wattwire/geolog/MainActivity.java
+++ app/src/main/java/net/wattwire/geolog/MainActivity.java
@@ -2,7 +2,9 @@ package net.wattwire.geolog;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -11,11 +13,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView mConditionText;
+    EditText mCustomText;
     Button mButtonSunny;
     Button mButtonRainy;
+    Button mButtonSubmit;
 
     // Get reference to "Root of Tree" in the Firebase .json file"
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
@@ -30,8 +36,12 @@ public class MainActivity extends AppCompatActivity {
         // Get UI Elements
 
         mConditionText = (TextView) findViewById(R.id.textCondition);
+        mCustomText =  (EditText) findViewById(R.id.textCustom);
+
         mButtonSunny = (Button) findViewById(R.id.buttonSunny);
         mButtonRainy = (Button) findViewById(R.id.buttonFoggy);
+        mButtonSubmit = (Button) findViewById(R.id.buttonSubmit);
+
     }
 
     @Override
@@ -52,16 +62,39 @@ public class MainActivity extends AppCompatActivity {
                     // ...So now let's synchronize dataSnapshot with the TextView conditionText
                     String ourText = dataSnapshot.getValue(String.class);
                     mConditionText.setText(ourText);
-
                 }
-
                 @Override
                 // "In case we run into any errors"...
                 public void onCancelled(DatabaseError databaseError) {
-
                 }
+
             });
 
+        mButtonSunny.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                mConditionRef.setValue("Sunny Days are Here!");
+            }
+        });
+        mButtonRainy.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mConditionRef.setValue("Raining Its Ass Off!");
+            }
+        });
+
+        mButtonSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+//               Text ourText = "Abc";
+
+                mConditionRef.setValue(mCustomText.getText().toString());
+
+//                mConditionRef.setValue("Test Text");
+            }
+        });
 
     }
 }
